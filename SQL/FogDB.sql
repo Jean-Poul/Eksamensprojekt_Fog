f337f0f283b2bddf81ce35b78ad129a6f7309801
Per <cph-pk171@cphbusiness.dk>
2020-04-27 18:21:07 +0200

    Nyt EER + FogDB.sql med item_list tabel

--- SQL/FogDB.sql
+++ SQL/FogDB.sql
@@ -1,9 +1,14 @@
--- MySQL Workbench Forward Engineering
+-- MySQL for FogDB
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+
+-- -----------------------------------------------------
+-- Schema fogdb
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `fogdb`;
 
 -- -----------------------------------------------------
 -- Schema fogdb
@@ -17,6 +22,22 @@ USE `fogdb` ;
 CREATE TABLE IF NOT EXISTS `fogdb`.`cities` (
   `zipcode` INT(11) NOT NULL,
   `cityname` VARCHAR(45) NOT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `fogdb`.`item_list`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `fogdb`.`item_list` (
+  `item_list_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `material_type` VARCHAR(45) NOT NULL,
+  `material` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(90) NULL DEFAULT NULL,
+  `pcs_per_unit` INT(11) NOT NULL,
+  `price` DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (`item_list_id`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
