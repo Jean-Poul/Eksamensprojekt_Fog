b686730583bb0b09bb5583c72f2fb166e14f172e
AlexanderPihl <cph-as509@cphbusiness.dk>
2020-05-08 13:50:17 +0200

    pushed wrong connector

--- src/main/java/DBAccess/Connector.java
+++ src/main/java/DBAccess/Connector.java
@@ -39,9 +39,9 @@ public class Connector {
             PASSWORD = System.getenv("JDBC_PASSWORD");
         } else {
             // Localhost
-            URL = "jdbc:mysql://localhost:3306/fogdb?serverTimezone=CET"; //&useSSL=false
+            URL = "jdbc:mysql://localhost:3306/fogdb?serverTimezone=CET&useSSL=false"; //&useSSL=false
             USERNAME = "root";
-            PASSWORD = "4354";
+            PASSWORD = "password";
         }
     }
 
